be34e50615e171f3704ea5203a6220220c0b70bc
unmarked4683 <pbuk60@gmail.com>
2025-01-19 21:38:31 +0100

    Added user docs

--- src/auth/dto/request/auth-login.dto.ts
+++ src/auth/dto/request/auth-login.dto.ts
@@ -5,6 +5,7 @@ export class AuthLoginDto {
   @ApiProperty({
     description: 'Email of user which tries to login',
     type: 'string',
+    example: 'test@foo.com',
   })
   @IsEmail()
   email: string;
@@ -12,6 +13,7 @@ export class AuthLoginDto {
   @ApiProperty({
     type: 'string',
     description: 'Password of user which tries to login',
+    example: 'MyVeryHardPassword123',
   })
   @IsString()
   password: string;
